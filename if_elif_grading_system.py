3917fa316148d061f4cb7bb0084c70af47655193
Ramashish Singh <singhramashish0502@gmail.com>
2021-07-25 21:52:28 +0530

    If elif for Grade system

---
+++ if_elif_grading_system.py
@@ -0,0 +1,13 @@
+num = int(input("enter number you got"))
+if num >90:
+    print('you are promoted and your Grade is "A+"')
+elif num>80 and num <90:
+    print('you are promoted and your Grade is "A"')
+elif num>70 and num <80 :
+    print("you are pass and your Grade is 'B'")
+elif num>60 and num <70 :
+    print("you are pass and your Grade is 'C'")
+elif num>50 and num <60 :
+    print("you are just  pass and your Grade is 'C-'")
+elif num>40 and num <50 :
+    print("Try harder and do your best")
