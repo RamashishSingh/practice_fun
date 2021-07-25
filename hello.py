85552841be74c50ab0f91d30f03908c9a72b9c36
Ramashish Singh <singhramashish0502@gmail.com>
2021-07-25 21:33:09 +0530

    folder for practice from root of python & github

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("hello this is Ramashish Singh")
+print('this is a practice set in which \nwe are going to do basic problems')
+print("""like in this we used three types of iverted commas for 
+            printing statements like single inverted comma 
+                                    double inverteed camma
+            and three double or single inverted commas \n which are used to write 
+                                     multiline comments \n but it is basically it is a string which is used to print
+                                     \n and also used for comments """)
