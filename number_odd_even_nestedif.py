5fce457cd7d8fabb7ca8e873105e2f0c31213652
Ramashish Singh <singhramashish0502@gmail.com>
2021-07-26 13:56:22 +0530

    just checked even number between given range

--- number_odd_even_nestedif.py
+++ number_odd_even_nestedif.py
@@ -7,3 +7,12 @@ if num >1:
         print(f'{num} is odd')
 else:
     print('please enter number greater than 1')
+
+#print even number between given range
+
+a = int(input('enter a number range start from '))
+j = int(input('enter a number range ends '))
+
+for i in range(a,j):
+    if(i%2 == 0):
+        print(i)
